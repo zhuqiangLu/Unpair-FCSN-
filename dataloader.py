6f857e703d83929d0fb13f838cc90307100b56aa
Zhuqiang Lu <rxontdkd@gmail.com>
2020-03-19 21:36:26 +0800

    remove dependency

--- dataloader.py
+++ dataloader.py
@@ -4,12 +4,36 @@ from torch.utils.data import Dataset
 import h5py
 
 
-class FeatureVectorLoader(Dataset):
+class FeatureVectorData(Dataset):
     def __init__(self, data_root):
-        self.data = h5py.file(data_root, 'r')
+
+        self.data = h5py.File(data_root, 'r')
+        self.keys = self.data.keys()
+        self._data = [i for i in range(len(self.data))]
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return 
+        idx += 1
+        key = 'video_{}'.format(idx)
+        video = self.data[key]
+        features = torch.t(torch.tensor(video['features']))
+        print(features.shape)
+        gtscore = torch.tensor(video['gtscore'][()], dtype=torch.long)
+        print(gtscore.shape)
+        return features
+
+
+def get_dataloader(path, batch_size=1):
+    train_dataset = FeatureVectorData(path)
+    loader = DataLoader(train_dataset, batch_size=batch_size)
+    return loader
+
+
+if __name__ == "__main__":
+    data_root = 'dataset/eccv16_dataset_summe_google_pool5.h5'
+    loader = get_dataloader(data_root)
+    for i, batch in enumerate(loader):
+        print(i)
+        print(batch.shape)

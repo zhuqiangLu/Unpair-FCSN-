307a379f3300a4128620dfb1c3656a5814789b85
root <rxontdkd@gmail.com>
2020-03-19 17:45:12 +0800

    update

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,15 @@
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 import h5py
 
 
-class FeatureVectorLoader()
+class FeatureVectorLoader(Dataset):
+    def __init__(self, data_root):
+        self.data = h5py.file(data_root, 'r')
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return 

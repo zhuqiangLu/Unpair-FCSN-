152de9b8908982cffcfd8cff79e0b028e3d7f677
Zhuqiang Lu <rxontdkd@gmail.com>
2020-03-19 15:39:10 +0800

    more dataset

--- getDataSet.sh
+++ getDataSet.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
 echo "downloading SumMe dataset"
-
-wget -P ./dataset https://data.vision.ee.ethz.ch/cvl/SumMe/SumMe.zip
+wget -c http://people.csail.mit.edu/yalesong/tvsum/tvsum50_ver_1_1.tgz
+wget -c  https://data.vision.ee.ethz.ch/cvl/SumMe/SumMe.zip

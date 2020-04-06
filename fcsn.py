da728d901f338ffbe431e6fb503aec6475407259
Zhuqiang Lu <rxontdkd@gmail.com>
2020-04-06 19:26:30 +0800

    add deconv layer

--- fcsn.py
+++ fcsn.py
@@ -12,7 +12,8 @@ class FCSN_ENC(nn.Module):
             nn.Conv1d(1024, 1024, 3, padding=1),  # conv1_2
             nn.BatchNorm1d(1024),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
+            # the temporal max pooling
+            nn.MaxPool1d(2, stride=2, ceil_mode=True),
         )
 
         # the length after conv1 will be ceiling((l-1)/2 + 1) near 1/2
@@ -24,7 +25,8 @@ class FCSN_ENC(nn.Module):
             nn.Conv1d(1024, 1024, 3, padding=1),  # conv2_2
             nn.BatchNorm1d(1024),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
+            # the temporal max pooling
+            nn.MaxPool1d(2, stride=2, ceil_mode=True),
         )
 
         # the length after conv1 will be ceiling((l-1)/2 + 1) near 1/4
@@ -39,7 +41,8 @@ class FCSN_ENC(nn.Module):
             nn.Conv1d(1024, 1024, 3, padding=1),  # conv3_3
             nn.BatchNorm1d(1024),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
+            # the temporal max pooling
+            nn.MaxPool1d(2, stride=2, ceil_mode=True),
         )
 
         # 1/8
@@ -54,7 +57,8 @@ class FCSN_ENC(nn.Module):
             nn.Conv1d(2048, 2048, 3, padding=1),  # conv4_3
             nn.BatchNorm1d(2048),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
+            # the temporal max pooling
+            nn.MaxPool1d(2, stride=2, ceil_mode=True),
         )
 
         # 1/16
@@ -74,7 +78,7 @@ class FCSN_ENC(nn.Module):
 
 
 class FCSN_MID(nn.Module):
-    def __init__(self, n_class=2):
+    def __init__(self, n_class):
         super(FCSN_MID, self).__init__()
 
         self.conv5 = nn.Sequential(
@@ -87,7 +91,8 @@ class FCSN_MID(nn.Module):
             nn.Conv1d(2048, 2048, 3, padding=1),  # conv4_3
             nn.BatchNorm1d(2048),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
+            # the temporal max pooling
+            nn.MaxPool1d(2, stride=2, ceil_mode=True),
         )
 
         # 1/32
@@ -124,7 +129,7 @@ class FCSN_MID(nn.Module):
 
 class FCSN(nn.Module):
 
-    def __init__(self, n_class=2):
+    def __init__(self, n_class=1024):
         super(FCSN, self).__init__()
 
         self.FCSN_ENC = FCSN_ENC()
@@ -151,148 +156,8 @@ class FCSN(nn.Module):
         h = self.FCSN_MID(h)
 
         upscore = self.deconv1(h)
+
         h = upscore + skip
-        h = self.deconv2(h)
-
-        return h
-
-
-class _FCSN(nn.Module):
-
-    '''
-    the input shape should be (n * c * l)
-    n: the number of tensor
-    c: the number of channel or the dim of feature vector
-    l: the length
-    '''
-
-    def __init__(self, n_class=2):
-        super(_FCSN, self).__init__()
-
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(1024, 1024, 3, padding=1),  # conv1_1
-            nn.BatchNorm1d(1024),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(1024, 1024, 3, padding=1),  # conv1_2
-            nn.BatchNorm1d(1024),
-            nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
-        )
-
-        # the length after conv1 will be ceiling((l-1)/2 + 1) near 1/2
-
-        self.conv2 = nn.Sequential(
-            nn.Conv1d(1024, 1024, 3, padding=1),  # conv2_1
-            nn.BatchNorm1d(1024),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(1024, 1024, 3, padding=1),  # conv2_2
-            nn.BatchNorm1d(1024),
-            nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
-        )
-
-        # the length after conv1 will be ceiling((l-1)/2 + 1) near 1/4
-
-        self.conv3 = nn.Sequential(
-            nn.Conv1d(1024, 1024, 3, padding=1),  # conv3_1
-            nn.BatchNorm1d(1024),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(1024, 1024, 3, padding=1),  # conv3_2
-            nn.BatchNorm1d(1024),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(1024, 1024, 3, padding=1),  # conv3_3
-            nn.BatchNorm1d(1024),
-            nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
-        )
-
-        # 1/8
-
-        self.conv4 = nn.Sequential(
-            nn.Conv1d(1024, 2048, 3, padding=1),  # conv4_1
-            nn.BatchNorm1d(2048),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(2048, 2048, 3, padding=1),  # conv4_2
-            nn.BatchNorm1d(2048),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(2048, 2048, 3, padding=1),  # conv4_3
-            nn.BatchNorm1d(2048),
-            nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
-        )
-
-        self.skip = nn.Sequential(
-            nn.Conv1d(2048, n_class, 1),
-            nn.BatchNorm1d(n_class),
-        )
-
-        # 1/16
-
-        self.conv5 = nn.Sequential(
-            nn.Conv1d(2048, 2048, 3, padding=1),  # conv4_1
-            nn.BatchNorm1d(2048),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(2048, 2048, 3, padding=1),  # conv4_2
-            nn.BatchNorm1d(2048),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(2048, 2048, 3, padding=1),  # conv4_3
-            nn.BatchNorm1d(2048),
-            nn.ReLU(inplace=True),
-            nn.MaxPool1d(2, 2, ceil_mode=True),  # the temporal max pooling
-        )
-
-        # 1/32
-
-        self.conv6 = nn.Sequential(
-            nn.Conv1d(2048, 4096, 1),
-            nn.BatchNorm1d(4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout()
-        )
-
-        self.conv7 = nn.Sequential(
-            nn.Conv1d(4096, 4096, 1),
-            nn.BatchNorm1d(4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout()
-        )
-
-        self.conv8 = nn.Sequential(  # the 1x1 cov layer
-            nn.Conv1d(4096, n_class, 3, padding=1),
-            nn.BatchNorm1d(n_class),
-            nn.ReLU(inplace=True),
-
-        )
-
-        self.deconv1 = nn.ConvTranspose1d(
-            n_class, n_class, 4, padding=1, stride=2, bias=False)
-        # the L_out_conv8 = L_out_conv5 as conv6/7/8 use 1x1 kernel
-        # L_out_conv5 = (L-1)/2^5 + 1
-        # L_out_deconv1 = (L_in -1) * stride - 2 * padding + dilation * (K-1)
-        # a skip connection will be established from the output of conv4 to the output of deconv1
-        # the setting of deconv1 ensure they have the same length
-        self.deconv2 = nn.ConvTranspose1d(
-            n_class, n_class, 16, stride=16, bias=False)
-
-    def forward(self, x):
-        h = x
-        h = self.conv1(h)
-        # print(h.shape)
-        h = self.conv2(h)
-        # print(h.shape)
-        h = self.conv3(h)
-        # print(h.shape)
-        h = self.conv4(h)
-        # print(h.shape)
-
-        skip = self.skip(h)
-
-        h = self.conv5(h)
-        h = self.conv6(h)
-        h = self.conv7(h)
-        h = self.conv8(h)
-
-        h = self.deconv1(h) + skip
         h = self.deconv2(h)
 
         return h
@@ -301,5 +166,5 @@ class _FCSN(nn.Module):
 if __name__ == '__main__':
     import torch
     net = FCSN()
-    data = torch.randn((2, 1024, 320))
+    data = torch.randn((1, 1024, 128))
     print(net(data).shape)
--- keyFrameSelector.py
+++ keyFrameSelector.py
@@ -1,27 +1,52 @@
 import torch.nn as nn
-
 from fcsn import FCSN
+import torch
 
 
 class SK(nn.Module):
-    def __init__(self, n_class=1):
+    def __init__(self, ratio=0.15, n_class=1024, T=320):
         super(SK, self).__init__()
-        self._SK = nn.Sequential(FCSN(n_class=n_class), nn.Sigmoid())
+
+        self.ratio = ratio
+        self._SK = FCSN(n_class=n_class)  # backbone fcsn
+        self.score_layer = nn.Sequential(  # to score the feature based on the decoded features
+            nn.Conv1d(n_class, 1, 1),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
-        skip = x
-        scores = self._SK(x)
+        video_features = x
 
+        k = int(x.shape[2] * self.ratio)
 
-        return scores
+        h = self._SK(x)
+
+        sk_out = h
+
+        scores = self.score_layer(h)  # shape (1,1,T)
+
+        top_scores = torch.topk(scores, k, dim=-1)
+        high = top_scores.values[0, 0, 0].data.cpu()
+        low = top_scores.values[0, 0, -1].data.cpu()
+        scores = torch.where(scores >= low, torch.ones(
+            scores.shape), torch.zeros(scores.shape))
+        h = sk_out * scores
+
+        # to remove the zeros rows
+        topk = torch.sum(h, dim=1)
+        picks = topk.nonzero(as_tuple=True)[1]  # all T that selected as S
+
+        s = h[:, :, picks]
+
+        # print(h[h.nonzero(as_tuple=True)].shape)
+        return s
 
 
 if __name__ == '__main__':
     import torch
-    net = SK(n_class=1)
-    data = torch.randn((1, 1024, 320))
-    out = net(data)
+    net = SK()
+    data = torch.randn((1, 1024, 128))
+    h = net(data)
+    print(h.shape)
     # import numpy as np
     # print(np.array(out.cpu().data[0]))
-    print(out.shape)
-    print(out[:,:,0:10])

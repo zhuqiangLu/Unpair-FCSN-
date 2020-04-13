d0f063adf8ba16912b04161dac394efda60831b3
Zhuqiang Lu <rxontdkd@gmail.com>
2020-04-13 14:33:50 +0800

    rework loader

--- dataloader.py
+++ dataloader.py
@@ -2,45 +2,120 @@ import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import h5py
+import random
 
 
-class FeatureVectorData(Dataset):
-    def __init__(self, data_root):
+class TrainData(Dataset):
+    def __init__(self, data_root, videos):
+
+        self.size = int(len(videos)/2)
+        self.V = random.sample(videos, self.size)
+        videos = [v for v in videos if v not in self.V]
+        self.S = random.sample(videos, self.size)
 
         self.data = h5py.File(data_root, 'r')
         self.keys = self.data.keys()
-        self._data = [i for i in range(len(self.data))]
 
     def __len__(self):
-        return len(self.data)
+        return self.size
 
     def __getitem__(self, idx):
-        idx += 1
-        key = 'video_{}'.format(idx)
+        #idx += 1
+
+        video = self.data[self.V[idx]]
+        features = torch.t(torch.tensor(video['features'][()]))
+
+        summary = self.data[self.S[idx]]
+        gt = torch.tensor(summary['gt_score'][()], dtype=torch.float)
+        print(self.V[idx], self.S[idx])
+        return features, gt
+
+
+class TestData(Dataset):
+    def __init__(self, data_root, videos):
+        self.videos = videos
+        self.data = h5py.File(data_root, 'r')
+        self.keys = self.data.keys()
+
+    def __len__(self):
+        return len(self.videos)
+
+    def __getitem__(self, idx):
+        #idx += 1
+        key = self.videos[idx]
         video = self.data[key]
-        features = torch.t(torch.tensor(video['features']))
+        features = torch.t(torch.tensor(video['features'][()]))
         # print(features.shape)
-        gtsummary = torch.tensor(video['user_summary'][()], dtype=torch.long)
+        gt = torch.tensor(video['gt_score'][()], dtype=torch.float)
         # print(gtscore.shape)
-        return features, gtsummary
+        return features, gt
 
 
-def get_dataloader(path, ratio, batch_size=1):
+class LoadersFactory():
+    def __init__(self, paths, ratio=0.8):
+        self.paths = paths
+        self.ratio = ratio
+        self.test_pair = dict()  # the test pair is fixed
+        self.train_pair = dict()
 
-    dataset = FeatureVectorData(path)
+        for path in paths:
+            keys = [key for key in h5py.File(path, 'r').keys()]
+            test_vids = random.sample(
+                keys, int(len(keys) * (1-self.ratio)))
+            self.test_pair[path] = test_vids
+            self.train_pair[path] = [
+                key for key in keys if key not in test_vids]
 
-    train_len = int(len(dataset) * ratio)
-    test_len = len(dataset) - train_len
+    def get_train_loaders(self, batch_size=1):
+        loaders = dict()
+        for k, v in self.train_pair.items():
+            loaders[k] = DataLoader(TrainData(k, v))
+        return loaders
 
-    train_dataset, test_dataset = torch.utils.data.random_split(
-        dataset, (train_len, test_len))
-    train_loader = DataLoader(train_dataset, batch_size=batch_size)
-    return train_loader, test_dataset
+    def get_test_data(self):
+        loaders = dict()
+        for k, v in self.test_pair.items():
+            loaders[k] = DataLoader(TestData(k, v))
+        return loaders
 
 
 if __name__ == "__main__":
-    data_root = 'FeatureVectors/eccv16_dataset_summe_google_pool5.h5'
-    train_loader, test_dataset = get_dataloader(data_root, ratio=1)
-    for i, feature, gt in enumerate(train_loader):
-        print(i)
-        print(batch.shape)
+    data_root = 'generated_data/summe.h5'
+    # train_loader, test_dataset = get_dataloader(data_root, ratio=0.8)
+    # for i, feature, gt in enumerate(train_loader):
+    #     print(i)
+    #     print(batch.shape)
+
+    # keys = [key for key in h5py.File(data_root, 'r').keys()]
+
+    # Train = random.sample(keys, int(len(keys)*0.8))
+    # Test = [key for key in keys if key not in Train]
+
+    # print("train", Train)
+    # print("test", Test)
+
+    # print("testdata")
+    # test_dataset = TestData(data_root, Test)
+    # test_loader = DataLoader(test_dataset, batch_size=1)
+    # for i, batch in enumerate(test_loader):
+    #     print(i)
+    #     print(batch[0].shape)
+    #     print(batch[1].shape)
+
+    # print("traindata")
+    # train_dataset = TrainData(data_root, Train)
+    # train_loader = DataLoader(train_dataset, batch_size=1)
+    # for i, batch in enumerate(train_loader):
+    #     print(i)
+    #     print(batch[0].shape)
+    #     print(batch[1].shape)
+
+    roots = ['generated_data/summe.h5', 'generated_data/tvsum.h5']
+    factory = LoadersFactory(roots)
+    loaders = factory.get_train_loaders()
+    for k, v in loaders.items():
+        print(k)
+        for i, batch in enumerate(v):
+            print(i)
+            print(batch[0].shape)
+            print(batch[1].shape)

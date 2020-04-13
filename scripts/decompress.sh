9fefae8b9f65649daff7640e3f9251108cfbf001
root <rxontdkd@gmail.com>
2020-04-13 20:41:04 +0800

    update script

--- scripts/decompress.sh
+++ scripts/decompress.sh
@@ -2,6 +2,8 @@
 
 tar zxvf ./RawVideos/tvsum.tgz  -C ./RawVideos/ 
 unzip ./RawVideos/summe.zip -d ./RawVideos/summe
+unzip ./RawVideos/ovp.zip -d ./RawVideos/
+unzip ./RawVideos/youtube.zip -d ./RawVideos/
 
 unzip ./RawVideos/ydata-tvsum50-v1_1/'*.zip' -d ./RawVideos/tvsum/
 rm -rf ./RawVideos/ydata-tvsum50-v1_1/
@@ -10,3 +12,6 @@ rm ./RawVideos/WebscopeReadMe.txt
 
 rm ./RawVideos/tvsum.tgz
 rm ./RawVideos/summe.zip
+rm ./RawVideos/ovp.zip
+rm ./RawVideos/youtube.zip
+
